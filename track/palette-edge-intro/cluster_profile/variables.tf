78be6125ba796d296721437c9a733660d53f6a92
vincentramirez <vinnie.ramirez@gmail.com>
2022-02-15 15:06:22 -0700

    adding terraform example

---
+++ track/palette-edge-intro/cluster_profile/variables.tf
@@ -0,0 +1,27 @@
+variable "sc_host" {
+  description = "Spectro Cloud Endpoint"
+  default     = "api.spectrocloud.com"
+}
+
+variable "sc_username" {
+  description = "Spectro Cloud Username"
+}
+
+variable "sc_password" {
+  description = "Spectro Cloud Password"
+  sensitive   = true
+}
+
+variable "sc_project_name" {
+  description = "Spectro Cloud Project (e.g: Default)"
+  default     = "Default"
+}
+
+#Cluster Profile
+variable "cluster_profile_name" {}
+variable "cluster_profile_description" {}
+variable "cluster_profile_tags" {}
+variable "cloud" {}
+variable "type" {}
+
+
