6b6284d18bdbb306ceae20ea4da1e34195247d24
Jesse-Ben <88511511+Jesse-Ben@users.noreply.github.com>
2023-03-27 13:29:50 -0400

    Updated pack names`

--- track/palette-terraform-aws/e2e/resource_clusterprofile.tf
+++ track/palette-terraform-aws/e2e/resource_clusterprofile.tf
@@ -1,51 +1,13 @@
-# If looking up a cluster profile instead of creating a new one
-# data "spectrocloud_cluster_profile" "profile" {
-#   # id = <uid>
-#   name = var.cluster_profile_name
-# }
-
-# # Example of a Basic add-on profile
-# resource "spectrocloud_cluster_profile" "cp-addon-aws" {
-#   name        = var.cluster_profile_name
-#   description = var.cluster_profile_description
-#   cloud       = var.cloud
-#   type        = var.type
-#   pack {
-#     name = "spectro-byo-manifest"
-#     tag  = "1.0.x"
-#     uid  = "5faad584f244cfe0b98cf489"
-#     # layer  = ""
-#     values = <<-EOT
-#       manifests:
-#         byo-manifest:
-#           contents: |
-#             # Add manifests here
-#             apiVersion: v1
-#             kind: Namespace
-#             metadata:
-#               labels:
-#                 app: wordpress
-#                 app3: wordpress3
-#               name: wordpress
-#     EOT
-#   }
-# }
 
 data "spectrocloud_registry" "registry" {
   name = "Public Repo"
 }
 
-#data "spectrocloud_pack" "argo" {
-  #name = "argo-cd"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version  = "3.26.7"
-#}
-
-#data "spectrocloud_pack" "k8sdash" {
-  #name = "k8s-dashboard"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version  = "2.4.0"
-#}
+data "spectrocloud_pack" "argo" {
+  name = "argo-cd"
+  registry_uid = data.spectrocloud_registry.registry.id
+  version  = "3.26.7"
+}
 
 data "spectrocloud_pack" "k8sdash" {
   name = "spectro-k8s-dashboard"
@@ -59,24 +21,11 @@ data "spectrocloud_pack" "k8sdash" {
   #version  = "3.7.0"
 #}
 
-
-#data "spectrocloud_pack" "csi" {
-  #name = "csi-aws"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version  = "1.0.0"
-#}
-
 data "spectrocloud_pack" "csi" {
-  name = "csi-aws-efs"
+  name = "csi-aws-ebs"
   registry_uid = data.spectrocloud_registry.registry.id
-  version  = "1.4.9"
+  version  = "1.16.0"
 }
-
-#data "spectrocloud_pack" "cni" {
-  #name    = "cni-calico"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version = "3.22.0"
-#}
 
 data "spectrocloud_pack" "cni" {
   name    = "cni-calico"
@@ -84,31 +33,17 @@ data "spectrocloud_pack" "cni" {
   version = "3.24.1"
 }
 
-#data "spectrocloud_pack" "k8s" {
-  #name    = "kubernetes"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version = "1.21.10"
-#}
-
 data "spectrocloud_pack" "k8s" {
   name    = "kubernetes"
   registry_uid = data.spectrocloud_registry.registry.id
   version = "1.25.4"
 }
 
-#data "spectrocloud_pack" "ubuntu" {
-  #name = "ubuntu-aws"
-  #registry_uid = data.spectrocloud_registry.registry.id
-  #version  = "20.04"
-#}
-
 data "spectrocloud_pack" "ubuntu" {
   name = "ubuntu-aws"
   registry_uid = data.spectrocloud_registry.registry.id
   version  = "22.04"
 }
-
-
 
 resource "spectrocloud_cluster_profile" "profile" {
   name        = var.sc_cp_profile_name
@@ -145,13 +80,6 @@ pack {
     values = data.spectrocloud_pack.csi.values
   }
 
-#pack {
-    #name   = data.spectrocloud_pack.opa.name
-    #tag    = data.spectrocloud_pack.opa.version
-    #uid    = data.spectrocloud_pack.opa.id
-    #values = data.spectrocloud_pack.opa.values
-  #}
-  
 pack {
     name   = data.spectrocloud_pack.k8sdash.name
     tag    = data.spectrocloud_pack.k8sdash.version
@@ -159,11 +87,11 @@ pack {
     values = data.spectrocloud_pack.k8sdash.values
   }  
 
-#pack {
-    #name   = "argo-cd"
-    #tag    = data.spectrocloud_pack.argo.version
-    #uid    = data.spectrocloud_pack.argo.id
-    #values = data.spectrocloud_pack.argo.values
-  #}  
+pack {
+    name   = "argo-cd"
+    tag    = data.spectrocloud_pack.argo.version
+    uid    = data.spectrocloud_pack.argo.id
+    values = data.spectrocloud_pack.argo.values
+  }  
 
 }
