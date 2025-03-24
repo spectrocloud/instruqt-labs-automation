220e5ecb87333db3795e435ea46430170a22f407
Kyle Jepson <kyle.jepson@spectrocloud.com>
2025-03-23 21:05:18 -0700

    added vertex folder for track

---
+++ track/palette-vertex-terraform-aws/resource_cluster.tf
@@ -0,0 +1,37 @@
+/* If using an existing cluster profile 
+
+data "spectrocloud_cluster_profile" "addon_profile" {
+  name = "<name-of-existing-cluster-profile>"
+}
+*/
+resource "spectrocloud_cluster_eks" "cluster" {
+  name = "tf-cluster01-eks"
+  tags = ["cloud:eks","env:prod"]
+  skip_completion = true
+  cloud_account_id = data.spectrocloud_cloudaccount_aws.account.id
+
+  cluster_profile {
+    id = spectrocloud_cluster_profile.infra_k8s-1-1-31.id
+  }
+
+  # cluster_profile {
+  #   id = spectrocloud_cluster_profile.add-on.id
+  # }
+
+  cloud_config {
+    ssh_key_name = var.aws_ssh_key_name
+    region       = var.aws_region
+    #vpc_id       = var.aws_vpc_id
+    #azs          = var.azs != [] ? var.azs : null
+    #az_subnets   = var.master_azs_subnets_map != {} ? var.master_azs_subnets_map : null
+  }
+
+  machine_pool {
+    name          = "worker-pool01"
+    count         = 3
+    instance_type = "t3.large"
+    #azs           = var.azs != [] ? var.azs : null
+    #az_subnets    = var.master_azs_subnets_map != {} ? var.master_azs_subnets_map : null
+    disk_size_gb  = 60
+  }
+}
---
+++ track/palette-vertex-terraform-aws/resource_clusterprofile-1-1-32.tf
@@ -0,0 +1,45 @@
+# If looking up a cluster profile instead of creating a new one
+# data "spectrocloud_cluster_profile" "profile" {
+#   # id = <uid>
+#   name = "eks-basic"
+# }
+#
+
+resource "spectrocloud_cluster_profile" "infra_k8s-1-1-32" {
+  name        = "tf-infra-eks-profile"
+  description = "basic eks cp"
+  cloud       = "eks"
+  type        = "infra"
+  tags        = ["cloud:eks"]
+  version     = "1.1.32"
+
+  pack {
+    name   = data.spectrocloud_pack.os.name
+    tag    = local.os_version
+    uid    = data.spectrocloud_pack.os.id
+    values = data.spectrocloud_pack.os.values
+  }
+
+  pack {
+    name   = data.spectrocloud_pack.k8s-1-1-32.name
+    tag    = local.k8s-1-1-32_version
+    uid    = data.spectrocloud_pack.k8s-1-1-32.id
+    values = data.spectrocloud_pack.k8s-1-1-32.values
+  }
+
+  pack {
+    name   = data.spectrocloud_pack.cni.name
+    tag    = local.cni_version
+    uid    = data.spectrocloud_pack.cni.id
+    values = data.spectrocloud_pack.cni.values
+  }
+
+  pack {
+    name   = data.spectrocloud_pack.csi.name
+    tag    = local.csi_version
+    uid    = data.spectrocloud_pack.csi.id
+    values = data.spectrocloud_pack.csi.values
+  }
+
+}
+
